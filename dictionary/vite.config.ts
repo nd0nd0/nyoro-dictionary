1ec8c4a056259de789012cb63d671df975e4277a
Micheal Ndondo <michealndo@gmail.com>
2025-11-22 12:26:15 +0300

    refactor: Remove direct Cloudflare Worker app and `@cloudflare/vite-plugin`, updating `wrangler.json` and `package.json` scripts for Cloudflare Pages deployment.

--- dictionary/vite.config.ts
+++ dictionary/vite.config.ts
@@ -2,16 +2,10 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [
-    cloudflare({ viteEnvironment: { name: "server" } }),
-    tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
-  ],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
